90538b9322e7bfa396d896b64c79bd28cd6b0cd9
HeSithGX2005 <hesarayasith9@gmail.com>
2024-09-08 23:21:25 +0530

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-route::get('post', function(){
-    return view('post');
+route::post('addproducts', function(){
+    return view('addproducts');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/addproducts', [ProductController::class, 'create'])->name('products.create');
+Route::post('/addproducts', [ProductController::class, 'store'])->name('products.store');
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
