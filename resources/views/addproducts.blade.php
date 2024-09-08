e0caca9609b3d1d373f3de6788c75d823bfa6e9f
HeSithGX2005 <hesarayasith9@gmail.com>
2024-09-08 23:19:37 +0530

    creates add product php

---
+++ resources/views/addproducts.blade.php
@@ -0,0 +1,40 @@
+@extends('adminlte::page')
+
+@section('title', 'Add Product')
+
+@section('content_header')
+    <h1>Add Product</h1>
+@stop
+
+@section('content')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('products.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="name">Product Name:</label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="form-group">
+            <label for="description">Description:</label>
+            <textarea class="form-control" id="description" name="description" required></textarea>
+        </div>
+        <div class="form-group">
+            <label for="price">Price:</label>
+            <input type="number" step="0.01" class="form-control" id="price" name="price" required>
+        </div>
+        <div class="form-group">
+            <label for="quantity">Quantity:</label>
+            <input type="number" class="form-control" id="quantity" name="quantity" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Add Product</button>
+    </form>
+@stop
