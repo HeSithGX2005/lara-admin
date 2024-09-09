e40e66f8c5904cb30fa5eb2d8553fac29b5a982d
HeSithGX2005 <hesarayasith9@gmail.com>
2024-09-09 14:09:40 +0530

    Created category controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(['name' => 'required']);
+        Category::create($request->only('name'));
+
+        return redirect()->route('categories.index')->with('success', 'Category added successfully.');
+    }
+
+    public function index()
+    {
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));
+    }
+
+    public function edit($id)
+    {
+        $category = Category::findOrFail($id);
+        return view('categories.edit', compact('category'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate(['name' => 'required']);
+        $category = Category::findOrFail($id);
+        $category->update($request->only('name'));
+
+        return redirect()->route('categories.index')->with('success', 'Category updated successfully.');
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return redirect()->route('categories.index')->with('success', 'Category deleted successfully.');
+    }
+}
+
